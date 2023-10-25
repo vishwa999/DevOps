2a92ec37741e6291c7d32389c821dfab29cfeabe
Vishwanath Maralappanavar - SKIDATA <vishwanath.maralappanavar@skidata.com>
2023-10-25 15:16:42 +0530

    first commit

---
+++ src/main/java/com/skidata/Sample.java
@@ -0,0 +1,13 @@
+package com.skidata;
+
+/**
+ * Hello world!
+ *
+ */
+public class Sample
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
